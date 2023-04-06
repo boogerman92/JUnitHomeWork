3287bfb42dc4f93687cd1a3c1523e79e8dc7bc10
Nazarenko A A <boogerman1992@gmail.com>
2023-04-06 13:56:42 +0400

    Initial gunit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,54 +1,10 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @Test
-    public void testPay100forBonus() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 900;
-
-        int actual = cashbackHackService.remain(amount);
-        int expected = 100;
-
-        assertEquals(actual, expected);
+    @org.junit.Test
+    public void remain() {
     }
-
-    @Test
-    public void testPay300forBonus() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 700;
-
-        int actual = cashbackHackService.remain(amount);
-        int expected = 300;
-
-        assertEquals(actual, expected);
-
-    }
-    @Test
-    public void testPay1000forBonus() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 0;
-
-        int actual = cashbackHackService.remain(amount);
-        int expected = 1000;
-
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    public void testPay0forBonus() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1000;
-
-        int actual = cashbackHackService.remain(amount);
-        int expected = 0;
-
-        assertEquals(actual, expected);
-    }
-
-
 }
